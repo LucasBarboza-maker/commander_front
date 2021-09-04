567eae5fcc3bd598c859cc0ee3bef17776c11ae8
LucasBarboza-maker <Lucasrb18@gmail.com>
2021-09-04 20:43:32 -0300

    Making filter input

--- Screens/Tables/index.js
+++ Screens/Tables/index.js
@@ -1,16 +1,22 @@
 import * as React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, Pressable} from 'react-native';
 
+import Utils from '../../assets/Utils/colors.json';
+import Fonts from '../../assets/Utils/fontSizes.json';
+
+import CommanderIconSvg from '../../components/SVGComponents/CommanderIconWhite';
 import GlobalStatusBar from '../../components/StatusBar/StatusBar.js';
+
+import {FilterInputText} from '../../components/Inputs/Text/InputText.js'
 
 
 export default function Tables() {
   return (
     <>
     <GlobalStatusBar/>
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Mesas</Text>
-    </View>
+      <View style={{paddingTop:10, height: 54, width: '100%', justifyContent: 'flex-start',  alignItems: 'center', backgroundColor: Utils.main_color, paddingLeft: 34, paddingRight: 34}}>
+        <FilterInputText width="100%" height={45}/>
+      </View>
     </>
   );
 }
--- components/Inputs/Text/InputText.js
+++ components/Inputs/Text/InputText.js
@@ -3,11 +3,11 @@ import {useState} from 'react';
 import Colors from '../../../assets/Utils/colors.json';
 import fonts from '../../../assets/Utils/fontSizes.json';
 import {View, StyleSheet, Pressable, TextInput} from 'react-native';
-import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
-import { faEye } from '@fortawesome/free-solid-svg-icons'
-import { faEyeSlash } from '@fortawesome/free-solid-svg-icons'
-
-
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
+import { faEye } from '@fortawesome/free-solid-svg-icons';
+import { faEyeSlash } from '@fortawesome/free-solid-svg-icons';
+import { faSearch} from '@fortawesome/free-solid-svg-icons';
+import { faBars} from '@fortawesome/free-solid-svg-icons';
 
 export function BasicTextInput({placeHolder, width, height, icon}) {
     return(
@@ -46,13 +46,41 @@ export function PasswordTextInput({placeHolder, width, height}) {
     )
 }
 
+export function FilterInputText({placeHolder, width, height}) {
+
+    const[visible, setVisible] = useState(true);
+    return(
+        <View style={{...styles.inputFilter,width: width, height:height, backgroundColor:'white'}}>
+            <FontAwesomeIcon onPress={()=> setVisible(false)} icon={ faSearch } size={32} style={{position:'absolute', left:5,  top:5, marginRight:10, color:Colors.text_opaque}} />
+            <TextInput
+            style={{fontSize:fonts.f17px, paddingLeft:40}}
+            onChangeText={()=> console.log("oi")}
+            placeholder={'Pesquisar'}
+            secureTextEntry={visible}
+            />
+            <FontAwesomeIcon onPress={()=> setVisible(false)} icon={ faBars } size={32} style={{position:'absolute', right:0,  top:5, marginRight:10, color:Colors.text_opaque}} />
+        </View>
+   
+    )
+}
+
 const styles = StyleSheet.create({
-  input: {
+   input: {
+        fontSize:fonts.f17px,
+        borderColor:Colors.border_opaque,
+        borderRadius:10,
+        margin: 12,
+        borderWidth: 1,
+        padding: 10,
+    },
+  inputFilter: {
+    position:'absolute',
+    top:25,
     fontSize:fonts.f17px,
     borderColor:Colors.border_opaque,
     borderRadius:10,
-    margin: 12,
     borderWidth: 1,
     padding: 10,
+    elevation:8
   },
 });
